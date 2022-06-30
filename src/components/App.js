51dae79f75ce9af057da8cf071c16540858122e7
Sachin patel <psachin375@gmail.com>
2022-06-30 07:51:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Project from ./Project;
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <Project data-ns-test="project-name"/>
+    <Project data-ns-test="project-description"/>
+    </div>
   )
 }
 
