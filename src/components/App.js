2bc750d631fd4b76b72b669734346e7b2c9d0b49
Sachin patel <psachin375@gmail.com>
2022-07-02 15:48:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">Amazon Clone</h1>
-      <p data-ns-test="project-description">Amazon clone in which user can register and login, see the products list, add to cart and remove from the cart when required</p>
+      <h1 data-ns-test="project-name">Google</h1>
+      <p data-ns-test="project-description">Google is a popular internet Search Engine. It scans the Web to find Web pages that are relevant to the words you have typed in the search box.</p>
     </div>
   )
 }
